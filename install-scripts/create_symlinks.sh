b67a5bf3542a4d614729aa9a7074165a2607b475
Vegard Bergsvik Øvstegård <vegovs@gmail.com>
2020-02-06 12:02:40 +0100

    :zap: Added wallpaper change and minor fixes

--- install-scripts/create_symlinks.sh
+++ install-scripts/create_symlinks.sh
@@ -4,6 +4,7 @@
 # Variables
 #==============
 dotfiles_dir=~/dotfiles
+log_file=~/install_progress_log.txt
 
 #==============
 # Symlinks
@@ -15,13 +16,22 @@ echo "Creating symlinks.."
 # Create symlinks in the home folder
 #==============
 ln -sf $dotfiles_dir/.vimrc ~/ &>/dev/null
+echo ".vimrc linked." >> $log_file
 ln -sf $dotfiles_dir/.bashrc ~/ &>/dev/null
+echo ".bashrc linked." >> $log_file
 ln -sf $dotfiles_dir/.bash_profile ~/ &>/dev/null
+echo ".bash_profile linked." >> $log_file
 ln -sf $dotfiles_dir/.gitconfig ~/ &>/dev/null
+echo ".gitconfig linked." >> $log_file
 ln -sf $dotfiles_dir/.tmux.conf ~/ &>/dev/null
+echo ".tmux linked." >> $log_file
 ln -sf $dotfiles_dir/.gitignore_global ~/ &>/dev/null
+echo ".gitignore_global linked." >> $log_file
 git config --global core.excludesfile ~/.gitignore_global &>/dev/null
+echo "Updated git global config with .gitignore_global." >> $log_file
 
 echo -e "\n====== Summary ======\n"
+cat $log_file
 echo
 echo "Enjoy"
+rm $log_file
