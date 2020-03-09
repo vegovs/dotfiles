53961b32dfb52158e0aa3db7b83d4cb906346af9
Vegard Bergsvik Øvstegård <vegovs@gmail.com>
2020-03-09 10:20:23 +0100

    :bug: Fixed swap caps:escape in setup not working

--- setup/setup.sh
+++ setup/setup.sh
@@ -29,8 +29,8 @@ func_install() {
             Yes)
                 xmodmap -e "keycode 9 = Caps_Lock NoSymbol Caps_Lock"
                 xmodmap -e "keycode 66 = Escape NoSymbol Escape"
-                xmodmap -e "keycode 9 = Caps_Lock NoSymbol Caps_Lock" >> ~/.bash_profile
-                xmodmap -e "keycode 66 = Escape NoSymbol Escape" >> ~/.bash_profile
+                echo -e "xmodmap -e \"keycode 9 = Caps_Lock NoSymbol Caps_Lock\"" >> ~/.bash_profile
+                echo -e "xmodmap -e \"keycode 66 = Escape NoSymbol Escape\"" >> ~/.bash_profile
                 break;
                 ;;
             No)
